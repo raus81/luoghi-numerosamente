59cb0af27a1d4b6abfd0db44b6eb6483a0bc09a9
Federico Cristiani <federico.cristiani@gmail.com>
2021-12-12 13:19:36 +0100

    Adsense

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     @stack('head')
-
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3475702324698098" crossorigin="anonymous"></script>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 

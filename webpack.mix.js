5648e43fe4e59b026da3a18f3913b005db788c10
Federico Cristiani <federico.cristiani@gmail.com>
2021-12-12 13:59:34 +0100

    Reduce css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
 /*
  |--------------------------------------------------------------------------
@@ -10,32 +11,8 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-let purgeCss = require('purgecss-webpack-plugin')
-let glob = require('glob-all')
 
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/main.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
-
-if (mix.inProduction()) {
-    mix.webpackConfig({
-        plugins: [
-            new purgeCss({
-                paths: glob.sync([
-                    path.join(__dirname, 'resources/views/**/*.blade.php')
-                ]),
-                extractors: [
-                    {
-                        extractor: class {
-                            static extract(content) {
-                                return content.match(/[A-z0-9-:\/]+/g)
-                            }
-                        },
-                        extensions: ['html', 'js', 'php', 'vue']
-                    }
-                ]
-            })
-        ]
-    })
-}

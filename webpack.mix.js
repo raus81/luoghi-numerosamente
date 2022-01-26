1f5fe0927dce8a0accdb1f044dd40dac3f827f94
Federico Cristiani <federico.cristiani@gmail.com>
2022-01-26 19:37:40 +0100

    Purge Css

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,4 +15,5 @@ require('laravel-mix-purgecss');
 mix.js('resources/js/app.js', 'public/js')
     .js('resources/js/main.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
+    .purgeCss()
     .sourceMaps();
